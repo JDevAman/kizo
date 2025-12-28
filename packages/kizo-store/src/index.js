0713d0e74eb6dae5ceb963fa8de782a74b7d084a
devaman <aman021276@gmail.com>
2025-12-28 17:41:02 +0000

    Fixes

--- packages/kizo-store/src/index.js
+++
@@ -1,9 +0,0 @@
-export { createStore } from "./createStore";
-export { default as authReducer } from "./slices/authSlice";
-export { setUser, logout, startLoading, } from "./slices/authSlice";
-export { default as uiReducer } from "./slices/uiSlice";
-export { addToast, removeToast } from "./slices/uiSlice";
-export { default as transactionReducer } from "./slices/transactionSlice";
-export { setTransactions, setError, setLoading, } from "./slices/transactionSlice";
-export { default as accountReducer } from "./slices/accountSlice";
-export { setAccount } from "./slices/accountSlice";
